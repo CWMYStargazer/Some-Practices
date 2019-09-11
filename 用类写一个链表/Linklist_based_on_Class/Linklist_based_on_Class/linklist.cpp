0319c99fa73cc88a3d4a2aa7886431ecb9ea6c25
2018081309027 <944329865@qq.com>
2019-09-11 13:10:43 +0800

    version 1.1

--- 用类写一个链表/Linklist_based_on_Class/Linklist_based_on_Class/linklist.cpp
+++ 用类写一个链表/Linklist_based_on_Class/Linklist_based_on_Class/linklist.cpp
@@ -1,12 +1,12 @@
+#include<stdio.h>
 #include<iostream>
 using namespace std;
 
 //对typedef的说明：给类型更名，比如struct Node又名ListNode，ListNode*又名ListNodePtr；
-//对ListPtr的说明：等效于ListNode*？有待考察...
+//对ListNode的说明：等效于ListNode*？有待考察...
 //感觉我根本没学过指针...（菜鸡落泪
 
 typedef int element;//存储的数据类型，这里习惯写成int；
-typedef ListNodePtr List, *ListPtr;
 
 typedef enum status//操作状态的枚举，书上这么教的，应该是定义返回类型？
 {
@@ -17,34 +17,33 @@ typedef struct Node//不清楚可以看一下c的链表作业，lv1_p11；
 {
 	element data;
 	struct Node *next;//指向下一个node的指针；
-}ListNode,*ListNodePtr;
+}ListNode;
 
 class linklist
 {
 public:
-	status list_head(ListPtr L);//创建新的空链表，形参L；
-	void list_destory(ListPtr L);//释放链表，形参L；
-	void list_clear(ListPtr L);//清空数据，顺便删了Node；
-	bool list_empty(ListPtr L);//判断是否只剩下头结点；
-	int list_size(ListPtr L);//求链表长度；
-	status list_pull(ListPtr L, element *elem, int point);//取出数据，需要知道在链表中的排序位置，返回该数据到elem中；
-	status list_locate(ListPtr L, element elem, int *point);//查找指定数据，返回该数据的node编号到point中；
-	status list_insert(ListPtr L, element elem, int point);//插入一个node，插入位置为point，自带元素elem；
-	status list_remove(ListPtr L, int point);//移除一个node，移除位置为point；
-	//status list_prior(ListPtr L, element *elem, int point);//求前驱，有需求在pull or locate的基础上修改；
-	//status list_next(ListPtr L, element *elem, int point);//求后继，有需求在pull or locate的基础上修改；
+	status list_head(ListNode *L);//创建新的空链表，形参L；
+	void list_destory(ListNode *L);//释放链表，形参L；
+	void list_clear(ListNode *L);//清空数据，顺便删了Node；
+	bool list_empty(ListNode *L);//判断是否只剩下头结点；
+	int list_size(ListNode *L);//求链表长度；
+	status list_pull(ListNode *L, element *elem, int point);//取出数据，需要知道在链表中的排序位置，返回该数据到elem中；
+	status list_locate(ListNode *L, element elem, int *point);//查找指定数据，返回该数据的node编号到point中；
+	status list_insert(ListNode *L, element elem, int point);//插入一个node，插入位置为point，自带元素elem；
+	status list_remove(ListNode *L, int point);//移除一个node，移除位置为point；
+	//status list_prior(ListNode L, element *elem, int point);//求前驱，有需求在pull or locate的基础上修改；
+	//status list_next(ListNode L, element *elem, int point);//求后继，有需求在pull or locate的基础上修改；
 private:
-	ListNodePtr p,q,s;//各种操作里面需要用的自由移动指针；
+	ListNode *p,*q,*s;//各种操作里面需要用的自由移动指针；
 	status result;//各种操作里面最后的结果；
 	int count;//计数器
 };
 
 //函数定义阶段
-status linklist::list_head(ListPtr L)
-{
+status linklist::list_head(ListNode *L){
 	result = fatal;
-	*L = new ListNode;
-	if (*L != NULL)
+	L = new ListNode;
+	if (L != NULL)
 	{
 		(*L).next = NULL;
 		result = success;
@@ -52,15 +51,13 @@ status linklist::list_head(ListPtr L)
 	return result;
 }
 
-void linklist::list_destory(ListPtr L)
-{
+void linklist::list_destory(ListNode *L){
 	list_clear(L);
-	delete (*L);
+	delete L;
 }
 
-void linklist::list_clear(ListPtr L)
-{
-	p = (*L);
+void linklist::list_clear(ListNode *L){
+	p = L;
 	q = p->next;
 	while (q)//从第二位开始删，链表始终不断；
 	{
@@ -70,16 +67,15 @@ void linklist::list_clear(ListPtr L)
 	}
 }
 
-bool linklist::list_empty(ListPtr L)
-{
-	p = (*L);
+bool linklist::list_empty(ListNode *L){
+	p = L;
 	if (p->next == NULL) return true;
 	else return false;
 }
 
-int linklist::list_size(ListPtr L)//头结点是不算在长度里面的；
-{
-	p = (*L);
+//头结点是不算在长度里面的；
+int linklist::list_size(ListNode *L){
+	p = L;
 	count = 0;
 	while (p != NULL)
 	{
@@ -89,10 +85,9 @@ int linklist::list_size(ListPtr L)//头结点是不算在长度里面的；
 	return count;
 }
 
-status linklist::list_pull(ListPtr L, element *elem, int point)
-{
+status linklist::list_pull(ListNode *L, element *elem, int point){
 	result = error;
-	p = (*L);
+	p = L;
 	count = 0;
 	while (p != NULL && count != point)
 	{
@@ -107,10 +102,9 @@ status linklist::list_pull(ListPtr L, element *elem, int point)
 	return result;
 }
 
-status linklist::list_locate(ListPtr L, element elem, int *point)
-{
+status linklist::list_locate(ListNode *L, element elem, int *point){
 	result = fail;
-	p = (*L);
+	p = L;
 	count = 0;
 	while (p != NULL)
 	{
@@ -128,12 +122,11 @@ status linklist::list_locate(ListPtr L, element elem, int *point)
 	return result;
 }
 
-status linklist::list_insert(ListPtr L, element elem, int point)
-{
+status linklist::list_insert(ListNode *L, element elem, int point){
 	result = error;
 	s = new ListNode;
 	s->data = elem;
-	p = (*L);
+	p = L;
 	count = 0;
 	while (p != NULL && count != point)
 	{
@@ -150,10 +143,9 @@ status linklist::list_insert(ListPtr L, element elem, int point)
 	return result;
 }
 
-status linklist::list_remove(ListPtr L, int point)
-{
+status linklist::list_remove(ListNode *L, int point){
 	result = error;
-	p = (*L);
+	p = L;
 	count = 0;
 	while (p != NULL && count != point - 1)
 	{
